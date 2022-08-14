1ebd0857aeb4a633feb230910d8c40ae331b1499
bhavaniomkarini <93519418+bhavaniomkarini@users.noreply.github.com>
2022-08-14 15:23:54 +0530

    Create pow(x,n).py

---
+++ pow(x,n).py
@@ -0,0 +1,22 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        
+        ans = float(1.0)
+        nn = n
+        
+        if nn < 0:
+            nn = -1 * nn
+            
+        while nn :
+            if nn % 2 :
+                ans = ans * x
+                nn = nn -1
+            else: 
+                x = x * x
+                nn = nn/2
+        if n < 0:
+            print("hi")
+            ans = 1.0/float(ans)
+            
+        return ans
+            
