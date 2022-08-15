db578e954961dd1656969f3abd75a9cbd0e0851b
bhavaniomkarini <93519418+bhavaniomkarini@users.noreply.github.com>
2022-08-15 17:23:43 +0530

    Update four_sum.py

--- four_sum.py
+++ four_sum.py
@@ -9,13 +9,13 @@ class Solution:
         
         nums = sorted(nums)
         
-        for i in range(len(nums)-3):
+        for i in range(n-3):
             
             #jumping over the duplicates
             if i>0 and nums[i]==nums[i-1]:
                 continue
                 
-            for j in range(i+1,len(nums)-2):
+            for j in range(i+1, n-2):
                 
                 #jumping over the duplicates
                 if j>i+1 and nums[j] == nums[j-1]:
