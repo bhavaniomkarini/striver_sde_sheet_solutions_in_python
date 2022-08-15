a246ed46fea87066ef3a35711ef7febfafea0b3c
bhavaniomkarini <93519418+bhavaniomkarini@users.noreply.github.com>
2022-08-15 17:24:40 +0530

    Update four_sum.py

--- four_sum.py
+++ four_sum.py
@@ -12,13 +12,13 @@ class Solution:
         for i in range(n-3):
             
             #jumping over the duplicates
-            if i>0 and nums[i]==nums[i-1]:
+            if i > 0 and nums[i]==nums[i-1]:
                 continue
                 
             for j in range(i+1, n-2):
                 
                 #jumping over the duplicates
-                if j>i+1 and nums[j] == nums[j-1]:
+                if j > i+1 and nums[j] == nums[j-1]:
                     continue
                     
                 target2 = target - nums[i] - nums[j]
